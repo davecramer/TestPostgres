6e5c86560e0297e522e1e893e3d4c87a43df9186
Dave Cramer <davecramer@gmail.com>
2020-06-30 07:27:13 -0400

    use /tmp as unix socket dir

--- src/main/groovy/Postgres.groovy
+++ src/main/groovy/Postgres.groovy
@@ -37,7 +37,7 @@ class Postgres {
         new Thread() {
             @Override
             void run() {
-                println "$binPath/postgres -h $hostName -k /var/run -p $port -i -D $dataPath"
+                println "$binPath/postgres -h $hostName -k /tmp -p $port -i -D $dataPath"
                 p = "$binPath/postgres -h $hostName -k /var/run -p $port -i -D $dataPath".execute(environment, null)
                 p.waitForProcessOutput(System.out, System.err)
 
